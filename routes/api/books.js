aecbf39921518530f00000b7b0ff75bc835ecd39
Flory Ann Evia <floryann.evia@gmail.com>
2020-12-03 17:22:39 -0600

    api routes edit

--- routes/api/books.js
+++ routes/api/books.js
@@ -1,16 +1,49 @@
 const router = require("express").Router();
-const booksController = require("../../controllers/booksController");
+const axios = require('axios');
 
-// Matches with "/api/books"
-router.route("/")
-  .get(booksController.findAll)
-  .post(booksController.create);
+const Book = require('../../models/book');
 
-// Matches with "/api/books/:id"
-router
-  .route("/:id")
-  .get(booksController.findById)
-  .put(booksController.update)
-  .delete(booksController.remove);
+router.get('/', (req, res) => {
+  Book.find()
+    .then(booksData => {
+      res.json(booksData);
+    })
+    .catch(err => {
+      res.json({ error: err });
+    });
+});
+
+router.post('/search', (req, res) => {
+  let search = req.body.search.replace(/\s/g, '+');
+
+  axios
+    .get(`https://www.googleapis.com/books/v1/volumes?q=${search}`)
+    .then(response => {
+      res.json(response.data.items);
+    })
+    .catch(err => {
+      res.json({ error: err });
+    });
+});
+
+router.post('/', (req, res) => {
+  Book.create(req.body)
+    .then(response => {
+      res.json({ successful: response });
+    })
+    .catch(err => {
+      res.json({ error: err });
+    });
+});
+
+router.delete('/:id', (req, res) => {
+  Book.findByIdAndDelete(req.params.id)
+    .then(response => {
+      res.json({ successful: response });
+    })
+    .catch(err => {
+      res.json({ error: err });
+    });
+});
 
 module.exports = router;
